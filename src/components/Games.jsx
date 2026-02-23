32e3bf4e892209e9ecb65f1ddce6771efcd906db
Gabriel Coutinho <gabrieljcoutinho@gmail.com>
2026-02-23 13:42:58 -0300

    teste

--- src/components/Games.jsx
+++ src/components/Games.jsx
@@ -12,6 +12,7 @@ const Games = () => {
             <div className="flexbox">
                 <div className="divFlexbox">
                   <img src={jogo1} alt="" className='imgFlexbox' />
+                  <span className='notaJogo'>4.9</span>
                   <hr className='linhaSobImg' />
                   <h2 className="tituloDoJogo">Cyber Chase</h2>
                     <p className="descricaoDoJogo">Action ° Open World</p>
