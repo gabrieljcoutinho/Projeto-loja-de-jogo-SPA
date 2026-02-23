b43337fa272b3453f9c775cb08ebdb58343953ce
Gabriel Coutinho <gabrieljcoutinho@gmail.com>
2026-02-23 13:15:08 -0300

    titul odos jogos

--- src/components/Games.jsx
+++ src/components/Games.jsx
@@ -12,15 +12,18 @@ const Games = () => {
             <div className="flexbox">
                 <div className="divFlexbox">
                   <img src={jogo1} alt="" className='imgFlexbox' />
+                  <h2 className="tituloDoJogo">Cyber Chase</h2>
 
                 </div>
 
                 <div className="divFlexbox">
                 <img src={jogo2} alt=""  className='imgFlexbox' />
+                <h2 className="tituloDoJogo">Mythic Quest</h2>
                 </div>
 
                 <div className="divFlexbox">
                 <img src={jogo3} alt=""  className='imgFlexbox' />
+                <h2 className="tituloDoJogo">Speed Racer</h2>
                 </div>
 
                 <div className="divFlexbox " id="divRanqueamento">
