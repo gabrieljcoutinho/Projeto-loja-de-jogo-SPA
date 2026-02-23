db086c77fc53faf1a25a3741f9ce3531428d0263
Gabriel Coutinho <gabrieljcoutinho@gmail.com>
2026-02-23 14:40:54 -0300

    imgs

--- src/components/Categorias.jsx
+++ src/components/Categorias.jsx
@@ -15,27 +15,27 @@ const Categorias = () => {
 
             <div className="flexboxCategoriasDeJogos">
                 <div className="categoriasJogos" id='action'>
-
+                        <img src={} alt="" />
                 </div>
 
                 <div className="categoriasJogos" id='rpg'>
-
+                <img src={} alt="" />
                 </div>
 
                 <div className="categoriasJogos" id='strategy'>
-
+                <img src={} alt="" />
                 </div>
 
                 <div className="categoriasJogos" id='indie'>
-
+                <img src={} alt="" />
                 </div>
 
                 <div className="categoriasJogos" id='multiplayer'>
-
+                <img src={} alt="" />
                 </div>
 
                 <div className="categoriasJogos" id='exclusiveLoot'>
-                    
+
                 </div>
             </div>
 
