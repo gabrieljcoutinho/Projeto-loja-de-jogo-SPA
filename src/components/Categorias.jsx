331af7933ed9a3f97b32b1fffd41163c46e71bd2
Gabriel Coutinho <gabrieljcoutinho@gmail.com>
2026-02-23 14:21:15 -0300

    Renomeand oconteudo das categorias de jogos

--- src/components/Categorias.jsx
+++ src/components/Categorias.jsx
@@ -11,7 +11,7 @@ const Categorias = () => {
                 <div className="categoriasJogos">Action</div>
                 <div className="categoriasJogos">RPG</div>
                 <div className="categoriasJogos">Strategy</div>
-                <div className="categoriasJogos">4</div>
+                <div className="categoriasJogos">Indie</div>
                 <div className="categoriasJogos">5</div>
                 <div className="categoriasJogos">6</div>
             </div>
