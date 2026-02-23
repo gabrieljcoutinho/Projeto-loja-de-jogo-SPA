6bdf4a4c5e4887973b83eb6d9856564d572491ec
Gabriel Coutinho <gabrieljcoutinho@gmail.com>
2026-02-23 10:15:00 -0300

    header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -6,6 +6,12 @@ const Header = () => {
   return (
     <div className='header'>
 
+        <ul className="lista-header">
+          <li className="itensLista"><a href="#" className='linksLista'></a></li>
+          <li className="itensLista"></li>
+          <li className="itensLista"></li>
+          <li className="itensLista"></li>
+        </ul>
 
 
     </div>
