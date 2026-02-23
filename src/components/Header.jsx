8751f9c047ba23b14acfb450f510472d8eee4a5d
Gabriel Coutinho <gabrieljcoutinho@gmail.com>
2026-02-23 10:20:49 -0300

    lista header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -7,10 +7,10 @@ const Header = () => {
     <div className='header'>
 
         <ul className="lista-header">
-          <li className="itensLista"><a href="#" className='linksLista'></a></li>
-          <li className="itensLista"><a href="#" className='linksLista'></a></li>
-          <li className="itensLista"><a href="#" className='linksLista'></a></li>
-          <li className="itensLista"><a href="#" className='linksLista'></a></li>
+          <li className="itensLista"><a href="#" className='linksLista'>Home</a></li>
+          <li className="itensLista"><a href="#" className='linksLista'>Store</a></li>
+          <li className="itensLista"><a href="#" className='linksLista'>Community</a></li>
+          <li className="itensLista"><a href="#" className='linksLista'>Profile</a></li>
         </ul>
 
 
