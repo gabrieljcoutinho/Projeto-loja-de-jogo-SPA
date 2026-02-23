580631047c0e480debfb373c6ec419da7bda6b16
Gabriel Coutinho <gabrieljcoutinho@gmail.com>
2026-02-23 14:25:38 -0300

    nome do id

--- src/components/Categorias.jsx
+++ src/components/Categorias.jsx
@@ -12,7 +12,7 @@ const Categorias = () => {
                 <div className="categoriasJogos" id='rpg'>RPG</div>
                 <div className="categoriasJogos" id='strategy'>Strategy</div>
                 <div className="categoriasJogos" id='indie'>Indie</div>
-                <div className="categoriasJogos" id=''>Multiplayer</div>
+                <div className="categoriasJogos" id='multiplayer'>Multiplayer</div>
                 <div className="categoriasJogos" id=''> Exclusive Loot</div>
             </div>
 
