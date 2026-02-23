0a8889335759c4b50a22d227148638c3e2f1b9bf
Gabriel Coutinho <gabrieljcoutinho@gmail.com>
2026-02-23 11:13:23 -0300

    css fundo

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,18 +1,23 @@
 import React from 'react'
 import "../../src/Css/home.css"
-import bgVideo from "../video/videoProjeto.mp4" // ajuste o caminho
+import bgVideo from "../video/videoProjeto.mp4" // ajuste se necessário
 
 const Home = () => {
   return (
-    <div className='home'>
+    <section className='home'>
+
       <video autoPlay loop muted playsInline className="background-video">
         <source src={bgVideo} type="video/mp4" />
       </video>
 
+      <div className="overlay"></div>
+
       <div className="content">
         <h1>Meu Site</h1>
+        <p>Visual moderno com vídeo de fundo</p>
       </div>
-    </div>
+
+    </section>
   )
 }
 
